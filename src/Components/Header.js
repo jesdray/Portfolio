dea200467f6cf1c42fa3ce66589da009db914aa5
Evgeniy <ttrrooll700@yandex.ru>
2022-07-10 16:41:06 +0700

    Инициалиация

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function header() {
+    return (
+    <header className="header">
+        <form className="header__form">
+            <button className="header__button" />
+            <input className="header__input" placeholder="React" ></input>
+        </form>
+    </header>
+    )
+}
