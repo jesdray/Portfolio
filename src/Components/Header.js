46de69da65021796193edf47cfae629bbb882af8
Евгений <ttrrooll700@yandex.ru>
2022-07-11 18:08:47 +0700

    инициализация

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,11 +2,21 @@ import React from "react";
 
 export default function header() {
     return (
-    <header className="header">
-        <form className="header__form">
-            <button className="header__button" />
-            <input className="header__input" placeholder="React" ></input>
-        </form>
-    </header>
+        <header className="header">
+            <form className="header__form">
+                <button className="header__button" />
+                <input className="header__input" placeholder="React" ></input>
+            </form>
+            <menu className="header__menu">
+                <ul className="header__ul">
+                    <li className="header__li">
+
+                    </li>
+                    <li className="header__li">
+
+                    </li>
+                </ul>
+            </menu>
+        </header>
     )
 }
